e2c58ef4556d596db83a4dd80b8db7ee14e0f8d3
smeth194243 <smor7432@740s-pl-106.local>
2017-05-10 12:34:23 -0600

    Thx isaac XD

--- src/ds/controller/DSController.java
+++ src/ds/controller/DSController.java
@@ -1,9 +1,44 @@
 package ds.controller;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+import ds.model.*;
+
 public class DSController
 {
+	private Queue<Customer> customerQueue;
+	private Stack<String> stackOfStrings;
+	
+	public DSController()
+	{
+		customerQueue = new LinkedList<Customer>();
+		stackOfStrings = new Stack<String>();
+	}
+	
+	
 	public void start()
 	{
+		Customer Isaac = new Customer("Isaac");
+		Customer Jacob = new Customer("Jacob");
+		Customer Tyler = new Customer("Tyler");
 		
+		customerQueue.add(Isaac);
+		customerQueue.add(Jacob);
+		customerQueue.add(Tyler);
+		
+		customerQueue.remove();
+		System.out.println(customerQueue.peek());
+		
+		String stringVariable = "This is a string that contains words.";
+		String variableString = "This is also a String that also contains words.";
+		
+		stackOfStrings.push(stringVariable);
+		stackOfStrings.push(variableString);
+		
+		stackOfStrings.pop();
+		
+		System.out.println(stackOfStrings.peek());
 	}
 }
---
+++ src/ds/model/Customer.java
@@ -0,0 +1,21 @@
+package ds.model;
+
+public class Customer 
+{
+private String name;
+	
+	public Customer(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public String toString()
+	{
+		return this.name;
+	}
+}
