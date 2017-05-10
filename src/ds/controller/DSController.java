f7714ad91204e8737ca5ed32bf2a82a0a76853e8
smeth194243 <smor7432@740s-pl-106.local>
2017-05-10 11:59:52 -0600

    Project created

---
+++ src/ds/controller/DSController.java
@@ -0,0 +1,9 @@
+package ds.controller;
+
+public class DSController
+{
+	public void start()
+	{
+		
+	}
+}
---
+++ src/ds/controller/DSRunner.java
@@ -0,0 +1,10 @@
+package ds.controller;
+
+public class DSRunner 
+{
+	public static void main(String []args)
+	{
+		DSController dsAppController = new DSController();
+		dsAppController.start();
+	}
+}
